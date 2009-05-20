7837461ab0db3564b0acf01f7fda8c020ef5d0a8
Justin Bozonier <darkxanthos@gmail.com>
2009-05-19 22:19:53 -0700

    First check in

---
+++ StringMatching/StringMatching/StringMatcher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringMatching
+{
+    public class StringMatcher
+    {
+        private const string _OffByOneLiteral = ".";
+        private const string _OffByManyLiteral = "*";
+
+        public static bool IsMatch(string pattern, string str)
+        {
+            var patternMatrix = _GetPatternMatrix(pattern, str);
+
+            return _HasCompletePath(patternMatrix, pattern.Length-1, str.Length-1, pattern);
+        }
+
+        private static bool _HasCompletePath(IList<List<bool>> matrix, int patternIndex, int stringIndex, string pattern)
+        {
+            var result = false;
+
+            if(patternIndex < 0 || stringIndex < 0)
+                return false;
+
+            if (patternIndex == 0 && stringIndex == 0 && matrix[patternIndex][stringIndex])
+            {
+                result = true;
+            }
+            else if(matrix[patternIndex][stringIndex])
+            {
+                var tempResult = _HasCompletePath(matrix, patternIndex - 1, stringIndex - 1, pattern);
+                if(!tempResult && _HasCompletePath(matrix, patternIndex, stringIndex-1, pattern) && pattern[patternIndex] == '*')
+                {
+                    tempResult = true;
+                }
+                result |= tempResult;
+            }
+
+            return result;
+        }
+
+        private static List<List<bool>> _GetPatternMatrix(string pattern, string str)
+        {
+            var patternMatrix = new List<List<bool>>(pattern.Length);
+            for (var i = 0; i < pattern.Length; i++)
+                patternMatrix.Add(new List<bool>(str.Length));
+
+            //First fill out table
+            foreach(var patternIndex in Enumerable.Range(0, pattern.Length))
+            {
+                var patternCharacter = pattern[patternIndex];
+
+                var isLiteralMatch = patternCharacter != '*' && patternCharacter != '.';
+                foreach(var stringIndex in Enumerable.Range(0, str.Length))
+                {
+                    var stringCharacter = str[stringIndex];
+
+                    if(isLiteralMatch)
+                    {
+                        patternMatrix[patternIndex].Add(_IsMatch(patternCharacter, stringCharacter));
+                    }
+                    else
+                    {
+                        patternMatrix[patternIndex].Add(true);
+                    }
+
+                }
+            }
+            return patternMatrix;
+        }
+
+        private static bool _IsMatch(char pattern, char str)
+        {
+            if (pattern == '*')
+                return true;
+            if(pattern == '.')
+                return true;
+            if(pattern == str)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ StringMatching/TestProject/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using StringMatching;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Simple_strings
+    {
+        [Test]
+        public void Simplest_strings_both_match()
+        {
+            Assert.IsTrue(StringMatcher.IsMatch("a", "a"));
+        }
+
+        [Test]
+        public void Simplest_strings_neither_match()
+        {
+            Assert.IsFalse(StringMatcher.IsMatch("a", "z"));
+        }
+
+        [Test]
+        public void Simplest_strings_simplest_pattern_matches()
+        {
+            Assert.IsTrue(StringMatcher.IsMatch(".", "z"));
+        }
+
+        [Test]
+        public void Simplest_strings_simplest_pattern_asterisk_matches()
+        {
+            Assert.IsTrue(StringMatcher.IsMatch("*", "z"));
+        }
+    }
+
+    [TestFixture]
+    public class Off_by_one_matchers
+    {
+        [Test]
+        public void String_off_by_one_case_1_matches()
+        {
+            Assert.IsTrue(StringMatcher.IsMatch("a.c.e", "abcde"));
+        }
+
+        [Test]
+        public void String_off_by_one_case_2_no_match()
+        {
+            Assert.IsFalse(StringMatcher.IsMatch("a.c.", "abcde"));
+        }
+
+    }
+
+    [TestFixture]
+    public class Off_by_many_matchers
+    {
+        [Test]
+        public void String_off_by_many_case_1_matches()
+        {
+            Assert.IsTrue(StringMatcher.IsMatch(".a*.j*", "cadeajmn"));
+        }
+
+        [Test]
+        public void String_off_by_many_case_2_no_match()
+        {
+            Assert.IsFalse(StringMatcher.IsMatch("a.d*", "abcde"));
+        }
+
+    }
+}
