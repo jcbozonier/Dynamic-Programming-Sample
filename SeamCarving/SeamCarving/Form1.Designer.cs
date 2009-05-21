4da809b17b4e817efc4ae3d329b7ca7d41d8bdbd
Justin Bozonier <darkxanthos@gmail.com>
2009-05-21 00:12:17 -0700

    First commit for seam carving project.

---
+++ SeamCarving/SeamCarving/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SeamCarving
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myPicture
+            // 
+            this.myPicture.Location = new System.Drawing.Point(12, 12);
+            this.myPicture.Name = "myPicture";
+            this.myPicture.Size = new System.Drawing.Size(456, 426);
+            this.myPicture.TabIndex = 0;
+            this.myPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 450);
+            this.Controls.Add(this.myPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.myPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox myPicture;
+    }
+}
+
---
+++ SeamCarving/SeamCarving/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeamCarving
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var bitmap = new Bitmap(@"C:\Code\DynamicProgramming\SeamCarving\Images\Desert_VerySimpleLandscape.jpg");
+            var newImage = ImagerBitmap.LaplaceGreyscale(bitmap);
+            var bounds = new Rectangle(Point.Empty, newImage.Size);
+
+            var imageEneryMatrix = new List<List<float>>(bounds.Width);
+
+            foreach(var xIndex in Enumerable.Range(0, bounds.Width))
+            {
+                var yIndexVector = new List<float>();
+                imageEneryMatrix.Add(yIndexVector);
+                foreach(var yIndex in Enumerable.Range(0, bounds.Height))
+                {
+                    yIndexVector.Add(newImage.GetPixel(xIndex, yIndex).GetBrightness());
+                }
+            }
+        }
+    }
+}
